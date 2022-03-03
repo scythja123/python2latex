f0fe86464e2d1c31458594624d2ff4e5bcfda00d
tgandor <tomasz.gandor@gmail.com>
2022-03-03 21:36:50 +0100

    reduced import nesting, added __all__ for module, updated README exmamples, formatted code with black.

--- python_to_latex/__init__.py
+++ python_to_latex/__init__.py
@@ -2,8 +2,10 @@
 This is a Python module that provides several functions to facilitate the inclusion of data produced in Python in a LaTex document
 
 """
-__authors__ = 'Sonja Stuedli'
-__author_emails__ = 'scythja@gmail.com'
-__version__ = '0.0.3'
+__authors__ = "Sonja Stuedli"
+__author_emails__ = "scythja@gmail.com"
+__version__ = "0.0.4"
 
-import python_to_latex
+from .python_to_latex import fig2pgf, mat2lat, numeric_list_to_tabularx
+
+__all__ = ["fig2pgf", "mat2lat", "numeric_list_to_tabularx"]
